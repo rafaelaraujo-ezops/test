72aca25221709713679ee28c2550d152b913b6cf
rafael <rafael.araujo@ezops.com.br>
2022-05-05 13:00:21 -0300

    test changelog

--- changelog.sh
+++ changelog.sh
@@ -5,7 +5,7 @@ HASH=$2
 
 echo "# Release Notes $now">wiki/changelog.md
 echo "## Project: core-api">>wiki/changelog.md
-echo "**commit:** [$STAGING_HASH]((https://github.com/rafaelaraujo-ezops/test/commit/$STAGING_HASH)">>wiki/changelog.md
+echo "**commit:** [$STAGING_HASH](https://github.com/rafaelaraujo-ezops/test/commit/$STAGING_HASH)">>wiki/changelog.md
 echo "### Features">>wiki/changelog.md
 git log $STAGING_HASH..$HASH -i -E --grep="^(feat)" --pretty=format:"**Author: %an** %n%n **commit message:** %s *at %ah* %n%n %N %n">>wiki/changelog.md 
 echo "### Fixes">>wiki/changelog.md
