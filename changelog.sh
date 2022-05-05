20d1381dbbb68ec16d818edaedab1039d7fa1f0e
rafaelaraujo-ezops <91761360+rafaelaraujo-ezops@users.noreply.github.com>
2022-05-05 17:23:03 -0300

    Style (#10)
    * testando
    
    * Teste AGAIN
    
    * testa aí de novo

--- changelog.sh
+++ changelog.sh
@@ -7,13 +7,13 @@ echo "# Release Notes $now">wiki/changelog.md
 echo "## Project: core-api">>wiki/changelog.md
 echo "**commit:** [$STAGING_HASH](https://github.com/rafaelaraujo-ezops/test/commit/$STAGING_HASH)">>wiki/changelog.md
 echo "### Features">>wiki/changelog.md
-git log $STAGING_HASH..$HASH -i -E --grep="^(feat)" --pretty=format:"**Author: %an** %n%n **commit message:** %s *at %ah* %n%n %N %n">>wiki/changelog.md 
+git log $STAGING_HASH..$HASH -i -E --grep="^(feat)" --merges  --pretty=format:"**Author: %an** %n%n **commit message:** %s  %n%n %N %n">>wiki/changelog.md 
 echo "### Fixes">>wiki/changelog.md
-git log $STAGING_HASH..$HASH -i -E --grep="^(fix)" --pretty=format:"**Author: %an** %n%n **commit message:** %s *at %ah* %n%n %N %n">>wiki/changelog.md
+git log $STAGING_HASH..$HASH -i -E --grep="^(fix)"  --merges --pretty=format:"**Author: %an** %n%n **commit message:** %s  %n%n %N %n">>wiki/changelog.md
 echo "### Styles">>wiki/changelog.md
-git log $STAGING_HASH..$HASH -i -E --grep="^(style)" --pretty=format:"**Author: %an** %n%n **commit message:** %s *at %ah* %n%n %N %n">>wiki/changelog.md
+git log $STAGING_HASH..$HASH -i -E --grep="^(style)" --merges  --pretty=format:"**Author: %an** %n%n **commit message:** %s  %n%n %N %n">>wiki/changelog.md
 echo "### OPS">>wiki/changelog.md
-git log $STAGING_HASH..$HASH -i -E --grep="^(ops)" --pretty=format:"**Author: %an** %n%n **commit message:** %s *at %ah* %n%n %N %n">>wiki/changelog.md
+git log $STAGING_HASH..$HASH -i -E --grep="^(ops)"  --merges --pretty=format:"**Author: %an** %n%n **commit message:** %s  %n%n %N %n">>wiki/changelog.md
 cat wiki/changelog.md
 
 
